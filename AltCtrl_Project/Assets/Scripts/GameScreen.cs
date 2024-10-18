c75bb0f2a6be61ee43876ebf0d7d1664b16db3e2
SopapillaKat <vellita2011@gmail.com>
2024-10-18 12:53:41 -0700

    buttons working

--- AltCtrl_Project/Assets/Scripts/GameScreen.cs
+++ AltCtrl_Project/Assets/Scripts/GameScreen.cs
@@ -4,8 +4,107 @@ using UnityEngine;
 
 public class GameScreen : MonoBehaviour
 {
+    public GameObject eyeReplacement;
+    public GameObject bandages;
+    public GameObject stitchUp;
+    public GameObject arteryBypass;
+    public GameObject heartListening;
+    public GameObject kidneyStone;
+    public GameObject cutOpen;
+    public GameObject weldOrgans;
     public void EyeReplacementButton()
     {
+        eyeReplacement.SetActive(true);
+        bandages.SetActive(false);
+        stitchUp.SetActive(false);
+        arteryBypass.SetActive(false);
+        heartListening.SetActive(false);
+        kidneyStone.SetActive(false);
+        cutOpen.SetActive(false);
+        weldOrgans.SetActive(false);
+    }
 
+    public void BandagesButton()
+    {
+        eyeReplacement.SetActive(false);
+        bandages.SetActive(true);
+        stitchUp.SetActive(false);
+        arteryBypass.SetActive(false);
+        heartListening.SetActive(false);
+        kidneyStone.SetActive(false);
+        cutOpen.SetActive(false);
+        weldOrgans.SetActive(false);
+    }
+
+    public void StitchUpButton()
+    {
+        eyeReplacement.SetActive(false);
+        bandages.SetActive(false);
+        stitchUp.SetActive(true);
+        arteryBypass.SetActive(false);
+        heartListening.SetActive(false);
+        kidneyStone.SetActive(false);
+        cutOpen.SetActive(false);
+        weldOrgans.SetActive(false);
+    }
+
+    public void ArteryBypassButton()
+    {
+        eyeReplacement.SetActive(false);
+        bandages.SetActive(false);
+        stitchUp.SetActive(false);
+        arteryBypass.SetActive(true);
+        heartListening.SetActive(false);
+        kidneyStone.SetActive(false);
+        cutOpen.SetActive(false);
+        weldOrgans.SetActive(false);
+    }
+
+    public void HeartListenButton()
+    {
+        eyeReplacement.SetActive(false);
+        bandages.SetActive(false);
+        stitchUp.SetActive(false);
+        arteryBypass.SetActive(false);
+        heartListening.SetActive(true);
+        kidneyStone.SetActive(false);
+        cutOpen.SetActive(false);
+        weldOrgans.SetActive(false);
+    }
+
+    public void KidneyStoneButton()
+    {
+        eyeReplacement.SetActive(false);
+        bandages.SetActive(false);
+        stitchUp.SetActive(false);
+        arteryBypass.SetActive(false);
+        heartListening.SetActive(false);
+        kidneyStone.SetActive(true);
+        cutOpen.SetActive(false);
+        weldOrgans.SetActive(false);
+    }
+
+    public void CutOpenButton()
+    {
+        eyeReplacement.SetActive(false);
+        bandages.SetActive(false);
+        stitchUp.SetActive(false);
+        arteryBypass.SetActive(false);
+        heartListening.SetActive(false);
+        kidneyStone.SetActive(false);
+        cutOpen.SetActive(true);
+        weldOrgans.SetActive(false);
+    }
+
+    public void WeldOrgansButton()
+    {
+        eyeReplacement.SetActive(false);
+        bandages.SetActive(false);
+        stitchUp.SetActive(false);
+        arteryBypass.SetActive(false);
+        heartListening.SetActive(false);
+        kidneyStone.SetActive(false);
+        cutOpen.SetActive(false);
+        weldOrgans.SetActive(true);
     }
 }
